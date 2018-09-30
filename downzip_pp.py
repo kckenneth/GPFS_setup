0e9124773a89a9fedf7487709a3acc9de690acba
Kenneth Chen <30378021+kckenneth@users.noreply.github.com>
2018-09-30 17:31:32 -0400

    Update downzip_pp.py

--- downzip_pp.py
+++ downzip_pp.py
@@ -7,28 +7,40 @@ from zipfile import ZipFile
 from urllib import urlopen 
 from collections import OrderedDict
 
+""" 
+This program will download English bigram repository at Google in 2009 and earlier. 
+Since the uncompressing the zipped file takes up 1.7GB for each file, the program will 
+preprocess on the fly during unzipping. It will also retrieve data: bigram and its count and 
+store them in binary format using pickle. 
+"""
+
 def generate_filenames(prefix, start, end):
-	filenames = []
-	start = int(start)
-	end = int(end)
+	"""This will generate filenames such as prefixstart.csv ...to prefixend.csv"""
+    
+    filenames = []
+    start = int(start)
+    end = int(end)
+    
+    for i in range(start, end+1, 1):
+        filename = ''.join([prefix, str(i), '.csv'])
+        filenames.append(filename)
+    return filenames
 
-	for i in range(start, end+1, 1):
-	    filename = ''.join([prefix, str(i), '.csv'])
-	    filenames.append(filename)
-	return filenames
-
-def generate_url(uri_base='', filename='', archive_type='.zip'):
-	return ''.join([uri_base, filename, archive_type])
+def generate_url(url_base='', filename='', archive_type='.zip'):
+    """This will generate url_basefilename.zip"""
+    return ''.join([url_base, filename, archive_type])
 
 def download_file(uri, filename):
-	url = urlopen(uri)
-	odic = OrderedDict()
+    
+    url = urlopen(uri)
+    odic = OrderedDict()
 	# retrieving essential data bigrams and its count on the fly
-	with ZipFile(StringIO(url.read())) as zipfile:
-		with zipfile.open(filename) as f:
-			for line in f:
-				fields = line.rstrip().split('\t')
-				try:
+    # filename = googlebooks-eng-all-2gram-20090715-0.csv ... googlebooks-eng-all-2gram-20090715-99.csv
+    with ZipFile(StringIO(url.read())) as zipfile:
+        with zipfile.open(filename) as f:
+            for line in f:
+                fields = line.rstrip().split('\t')
+                try:
 					# only keep standard English characters
                     # k = bigram key, v = bigram count value
 					k = fields[0].encode('utf8').decode('ascii').lower()
@@ -37,14 +49,11 @@ def download_file(uri, filename):
 				else:
 					v = int(fields[2])
 					if k in odic:
-						odic[k] += v
+						odic[k] += v       # keep adding the match count if the bigram exists
 					else:
-						odic[k] = v
+						odic[k] = v        # assign a new match count if the bigram does not exist yet
 	return odic
 
-def save_file(input_data, output_file):
-	with open(output_file, 'wb') as of:
-		pickle.dump(input_data, of)
 
 if __name__ == '__main__':
 
@@ -57,16 +66,17 @@ if __name__ == '__main__':
 
 		names = generate_filenames('googlebooks-eng-all-2gram-20090715-', 67, 99)
 
-		ub = 'http://storage.googleapis.com/books/ngrams/books/'
-		at = '.zip'
+		url_base = 'http://storage.googleapis.com/books/ngrams/books/'
+		file_ext = '.zip'
 
 		target_dir=''
 		for i, name in enumerate(names, start=67):
-			url_str = generate_url(ub, name, at)
+			url_str = generate_url(url_base, name, file_ext)
 			print 'downloading and parsing file from %s\n' % (url_str)
 			this_dict = download_file(url_str, name)
 
 			output_filename = ''.join(['bigrams-', str(i), '.txt'])
 			print 'writing file... %s' % (output_filename)
+            # saving {"bigram":"count"} dict in binary format
 			with open(output_filename, 'wb') as outputfile:
 				pickle.dump(this_dict, outputfile)
